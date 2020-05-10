bad58663cc134c230ed6077122badec61dd24652
BytteL <lettybedard@gmail.com>
2020-05-10 11:12:12 -0400

    table displaying, no format or heading

--- src/App.js
+++ src/App.js
@@ -2,25 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const data = 
+[
+  {
+    "pic": "./data/images/susan.jpg",
+    "firstName": "Susan",
+    "lastName": "Smith",
+    "email": "susieq420@hotmail.com", 
+    "phone": 2025551234
+  },{
+    "pic": "https://loremflickr.com/100/100",
+    "firstName": "Susan",
+    "lastName": "Smith",
+    "email": "susieq420@hotmail.com", 
+    "phone": 2025551234
+  }
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table className = "table">
+        <thead>
+          <tr>    
+            <th scope = "col"></th>
+            <th scope = "col">First Name</th>
+            <th scope = "col">Last Name</th>
+            <th scope = "col">email</th>
+            <th scope = "col">phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((employee) => {
+              const { pic, firstName, lastName, email, phone } = employee;
+              return (
+                <tr>
+                  <td><img src={pic} alt=""></img></td>
+                  <td>{firstName}</td>
+                  <td>{lastName}</td>
+                  <td>{email}</td>
+                  <td>{phone}</td>
+                </tr>
+              )
+            })
+          } 
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
