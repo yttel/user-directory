0e5740f47366bf53898839b8f6db218c2dae0f00
BytteL <lettybedard@gmail.com>
2020-05-10 21:14:13 -0400

    Updates

---
+++ precache-manifest.d12e1a9f46e00fdd92bf9a131fc72b45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03ffcc35e3bb9e411782e3ffafeab82",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "be7f7eaa09720087fbdd",
+    "url": "/user-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83362cdc20b545adeeca",
+    "url": "/user-directory/static/js/2.35dad609.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.35dad609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be7f7eaa09720087fbdd",
+    "url": "/user-directory/static/js/main.fe3312aa.chunk.js"
+  },
+  {
+    "revision": "a1b339b7bcd64c5592e1",
+    "url": "/user-directory/static/js/runtime-main.aeb176e3.js"
+  }
+]);
